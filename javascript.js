564b61d1f17f42e8c03ccbdbaa02f4a310b3e0ac
Valerio Varvaglione <vvalerio94@hotmail.it>
2023-03-08 18:26:22 +0100

    Secondo commit

---
+++ javascript.js
@@ -0,0 +1,37 @@
+let number1 = 10
+let number2 = 20
+let number3= 30
+let number4 = 40
+let number5 = 50
+
+let sum = number1 + number2 + number3 + number4 + number5;
+let media = sum / 5;
+
+
+
+console.log("La somma tra i numeri equivale a "+ sum);
+
+
+
+let currentyear = 2023
+let dateofbirth =  1994
+let age = currentyear - dateofbirth
+
+console.log(`ho ${age} e mi mancano ${100-age} per compierne 100`);
+
+
+
+
+let totcats = 44
+let linecats = 6
+let rest = totcats % linecats
+let x = linecats - rest
+let line = parseInt(totcats / linecats)
+
+console.log(`Ci sono ${line} file di gatti e ne mancano ${x} per una nuova fila, con un avanzo di ${rest}`);
+
+
+
+
+
+
